74fba9f6f702a343cd4dd6a0ec19e24c7b5b4878
RstTiwari <iamrrt2121@gmail.com>
2023-03-28 00:01:10 +0530

    added changes

--- src/API/env.js
+++ src/API/env.js
@@ -3,9 +3,9 @@ let prod = true
 export const env = ()=>{
     let url = ""
     if(prod){
-       url = "http://161.35.22.107:4000/api/";
+       url = "http://161.35.22.107:4000/";
     }else{
-        url = "http://localhost:4000/api/";
+        url = "http://localhost:4000/";
     }
     return url
 }
