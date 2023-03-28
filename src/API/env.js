c217df4ed9ede80c46db604439cc99cdab231be0
RstTiwari <iamrrt2121@gmail.com>
2023-03-28 21:49:53 +0530

    added new env

--- src/API/env.js
+++ src/API/env.js
@@ -3,7 +3,7 @@ let prod = true
 export const env = ()=>{
     let url = ""
     if(prod){
-       url = "https://myfac8ryapi.vercel.app/api/";
+       url = "https://myfac8ry.com/myfac8ry/api/";
     }else{
         url = "http://localhost:4000/";
     }
