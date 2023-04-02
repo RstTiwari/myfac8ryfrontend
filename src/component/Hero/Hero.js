c096243e01686552ca436342232574077646691a
RstTiwari <iamrrt2121@gmail.com>
2023-04-02 20:09:04 +0530

    added button name

--- src/component/Hero/Hero.js
+++ src/component/Hero/Hero.js
@@ -13,18 +13,18 @@ const Hero = () => {
     <div className="home">
       <div className="carouselDiv">
         <div className="aieffectDiv">
-          <h1 className="aitext">AI Cloud  Industrial Solution </h1>
+          <h1 className="aitext">AI Cloud Industrial Solution </h1>
           <p>
             With our AI solution,Keep track of Production,Inventory Quality.
-            <span> MyFac8ry AI Powered  help you in </span> ,
+            <span> MyFac8ry AI Powered help you in </span> ,
             <span> Increase Productivity,</span>
             <span> Decrese Inventory,</span>
             <span> Improve Quality ,</span>
           </p>
           <div className="quotation_button">
             <div className="hero__landing__logo">
-              <a href="/" className="instant__quote__button">
-                Start Your Free Trial
+              <a href="/customerarea" className="instant__quote__button">
+                Start Using
               </a>
             </div>
           </div>
