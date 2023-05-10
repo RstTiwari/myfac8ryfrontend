a2e05dd8ba01f594d13a87bb643976f913cf2a29
RstTiwari <iamrrt2121@gmail.com>
2023-05-11 00:23:38 +0530

    removed s

--- src/API/env.js
+++ src/API/env.js
@@ -3,7 +3,7 @@ let prod = true
 export const env = ()=>{
     let url = ""
     if(prod){
-       url = "https://170.64.178.125/api/";
+       url = "http://170.64.178.125/api/";
     }else{
         url = "http://localhost:4000/";
     }
