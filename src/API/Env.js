f2a509d73a64a9486685ca46478cf8c143de6eb3
RstTiwari <iamrrt2121@gmail.com>
2023-07-16 15:46:38 +0530

    added

--- src/API/Env.js
+++ src/API/Env.js
@@ -1,5 +1,5 @@
 export const Env = () => {
-  let apiUrl = "https://myfac8ry.com:4000/api/";
+  let apiUrl = "https://147.182.141.127:4000/api/";
   if (process.env.NODE_ENV === "development") {
     apiUrl = "http://localhost:4000/api/";
   }
