95ea9c5dfd5d97fc58a2719da53e78acdab0f0aa
RstTiwari <iamrrt2121@gmail.com>
2023-08-05 22:59:55 +0530

    adde wtsp icon

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from "./component/Header/Header";
+import  "./index.css"
 import Hero from "./component/Hero/Hero";
 import IndustrialCard from "./component/IndustrialCards/IndustrialCard";
 import Turbocharge from "./component/Turbocharge/Turbocharge";
 import Footer from './component/Footer/Footer';
 import Orderprocess from './component/Orderprocess/Orderprocess';
+import Wtsplogo from "./Assets/myfac8ryWtsp.svg"
 
 const Home = () => {
   return (
@@ -12,6 +14,13 @@ const Home = () => {
       <>
         <Header />
         <Hero />
+
+        <div className="wtsp__img">
+          <a href="https://wa.me/+918767948130">
+            <img className="wtsp__img__icon" alt="wtsp" src={Wtsplogo} />
+          </a>
+        </div>
+
         <IndustrialCard />
         <Turbocharge />
         <Footer />
