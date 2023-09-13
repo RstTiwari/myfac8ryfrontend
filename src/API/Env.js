b76529ba2287fcd8ae90f928135c3cb330289e0d
RstTiwari <iamrrt2121@gmail.com>
2023-09-13 16:57:10 +0530

    ADDED NEW ENV URL

--- src/API/Env.js
+++ src/API/Env.js
@@ -1,5 +1,5 @@
 export const Env = () => {
-  let apiUrl = "https://myfac8ry.com/fac8ry/";
+  let apiUrl = "https://myfac8ryapi.vercel.app/";
   if (process.env.NODE_ENV === "development") {
     apiUrl = "http://localhost:4000/"
   }
